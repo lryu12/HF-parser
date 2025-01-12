aa83c7b6c1334f35974082abdea920be9ead34d3
Louis Ryu <ryulouis8@gmail.com>
2025-01-12 14:24:02 -0800

    only using SheetJS

--- utils/ssParser.js
+++ utils/ssParser.js
@@ -1,22 +1,49 @@
 const XLSX = require('xlsx');
-const HyperFormula = require('hyperformula').HyperFormula;
+
+function extractColumnBasedFormulas(filePath, targetRow) {
+  const workbook = XLSX.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+
+  const formulasForRow = {};
+
+  for (const cellAddress in sheet) {
+    if (cellAddress[0] === '!') continue;
+
+    const cell = sheet[cellAddress];
+    const rowNumber = parseInt(cellAddress.replace(/^\D+/, ''), 10);
+
+    if (rowNumber === targetRow && cell.f) {
+      const columnLetter = cellAddress.replace(/\d+$/, '');
+
+      const adjustedFormula = cell.f.replace(/[A-Z]+\d+/g, match => {
+        return match.replace(/\d+$/, '');
+      });
+
+      formulasForRow[columnLetter] = adjustedFormula;
+    }
+  }
+
+  return formulasForRow;
+}
+
 
 async function parseSpreadsheet(filePath) {
   const workbook = XLSX.readFile(filePath);
   const sheetName = workbook.SheetNames[0];
   const sheet = workbook.Sheets[sheetName];
-  const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-  const hfInstance = HyperFormula.buildFromArray(jsonData);
+  const sheetArray = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+  const formulaArray = XLSX.utils.sheet_to_formulae(sheet);
+  console.log(formulaArray);
 
-  const parsedData = hfInstance.getSheetValues(0).map((row, rowIndex) => {
-    return row.map((value, colIndex) => {
-      const formula = hfInstance.getCellFormula({ sheet: 0, row: rowIndex, col: colIndex });
-      return formula ? { formula, value } : value;
-    });
-  });
+  const headers = sheetArray[3];
+  const formulas = extractColumnBasedFormulas(filePath, 5);
 
-  return parsedData;
+  console.log('Headers:', headers);
+  console.log('Formulas in first row:', formulas);
+
+  return { headers, formulas };
 }
 
 module.exports = parseSpreadsheet;
